3fa5fb812cc7bd726cf6e843eec4c0709a456bc8
yajiv185 <yajiv185@gmail.com>
2018-10-19 10:57:10 +0530

    added some files

---
+++ SimpleRestApiEntity/CarInputParams.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation.Attributes;
+
+namespace SimpleRestApiEntity
+{
+    [Validator(typeof())]
+    public class CarInputParams
+    {
+        public int MakeId { get; set; }
+        public int ModelId { get; set; }
+        public string Fuel { get; set; }
+        public double Price { get; set; }
+        public int Km { get; set; }
+        public int Year { get; set; }
+        public int SellerId { get; set; }
+    }
+}
---
+++ SimpleRestApiEntity/SellerInputParams.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Attributes;
+
+namespace SimpleRestApiEntity
+{
+    [Validator(typeof())]
+    public class SellerInputParams
+    {
+        public string SellerName { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string SellerAddress { get; set; }
+        public int CityId { get; set; }
+    }
+}
