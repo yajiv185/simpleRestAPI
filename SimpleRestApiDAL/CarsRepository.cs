dab79f3eb7f67314503c300c14da480730b9c64c
yajiv185 <yajiv185@gmail.com>
2018-10-19 10:57:10 +0530

    added some files

---
+++ SimpleRestApiDAL/CarsRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+
+namespace SimpleRestApiDAL
+{
+    public class CarsRepository
+    {
+        private string _connString = ConfigurationManager.ConnectionStrings["DatabaseConncet"].ConnectionString;
+        public void InsertCars()
+        {
+            //ReadStock stockDetail = new ReadStock();
+            try
+            {
+                using (IDbConnection conn = new MySqlConnection(_connString))
+                {
+                    stockDetail = conn.Query<ReadStock>("sp_UsedCarsCreate", stock, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return stockDetail;
+        }
+    }
+}
